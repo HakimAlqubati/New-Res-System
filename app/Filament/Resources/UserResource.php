ed19dd9e7d6ed6136f548fd98f2bd5ba4bf3f43d
Hakeem Qubati <hakimahmed123321@gmail.com>
2023-06-02 04:02:46 +0300

    add user resource for admin panel

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,16 +2,20 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\UserResource\Pages; 
-use App\Models\User; 
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
-use Filament\Tables; 
-use Filament\Tables\Columns\TextColumn; 
-use Illuminate\Database\Eloquent\Builder; 
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
+
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
@@ -23,15 +27,22 @@ class UserResource extends Resource
         return $form
             ->schema([
 
-                TextInput::make('name'),
-                TextInput::make('email'),
+                TextInput::make('name')->required(),
+                TextInput::make('email')->email()->required(),
 
                 TextInput::make('password')
                     ->password()
-                    ->reactive(),
-                TextInput::make('passwordConfirmation')
-                    ->password()
-                    ->dehydrated(false),
+                    ->required()
+                    ->reactive()
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state)),
+                Select::make('role_id')
+                    ->searchable()
+                    ->required()
+                    ->options(function () {
+                        return DB::table('roles')->pluck('name', 'id');
+                    })->hiddenOn(Pages\EditUser::class)
+
+
             ]);
     }
 
--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -5,8 +5,27 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\DB;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $user_id = $this->record->id;
+        $role_id = $this->data['role_id'];
+        DB::table('model_has_roles')->insert([
+            'role_id' => $role_id,
+            'model_id' => $user_id,
+            'model_type' => 'App\Models\User'
+        ]);
+    }
+
+    // protected function mutateFormDataBeforeSave(array $data): array
+    // {
+    //     dd($data);
+    //     return $data;
+    // }
+
 }
