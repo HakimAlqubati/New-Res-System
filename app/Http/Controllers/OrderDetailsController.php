1357c962ac3055d5f1d10d61201c4317da79162c
Hakeem Qubati <hakimahmed123321@gmail.com>
2023-06-08 19:08:28 +0300

    convert update order details to multi request

--- app/Http/Controllers/OrderDetailsController.php
+++ app/Http/Controllers/OrderDetailsController.php
@@ -76,44 +76,49 @@ class OrderDetailsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        try {
-            // Start a database transaction.
-            DB::beginTransaction();
+        // Validate the request data.
+        $data = $request->validate([
+            'orders_details.*.id' => ['required', 'integer', Rule::exists('orders_details', 'id')],
+            'orders_details.*.product_id' => ['required', 'integer', Rule::exists('products', 'id')->where('active', 1)],
+            'orders_details.*.unit_id' => ['required', 'integer', Rule::exists('units', 'id')],
+            'orders_details.*.quantity' => ['required', 'numeric', 'min:0'],
+            'orders_details.*.available_quantity' => ['required', 'numeric', 'min:0'],
+            'orders_details.*.available_in_store' => ['required', 'numeric', 'min:0'],
+            'orders_details.*.operation' => ['required', 'string', Rule::in(['update', 'destroy'])],
+        ]);
 
+        // Initialize an array to hold the responses.
+        $responses = [];
+
+        // Loop through the order detail IDs and update or delete each one.
+        foreach ($data['orders_details'] as $orderDetailData) {
             // Find the order detail with the specified ID.
-            $orderDetail = OrderDetails::findOrFail($id);
-
-            // Get the input data from the request.
-            $data = $request->validate([
-                'product_id' => ['required', 'integer', Rule::exists('products', 'id')->where('active', 1)],
-                'unit_id' => ['required', 'integer', Rule::exists('units', 'id')],
-                'quantity' => ['required', 'numeric', 'min:0'],
-                'available_quantity' => ['required', 'numeric', 'min:0'],
-                'available_in_store' => ['required', 'numeric', 'min:0'],
-                'operation' => ['required', 'string', Rule::in(['update', 'destroy'])],
-            ]);
+            $orderDetail = OrderDetails::findOrFail($orderDetailData['id']);
 
             // Find the new product by ID.
-            $product = Product::findOrFail($data['product_id']);
+            $product = Product::findOrFail($orderDetailData['product_id']);
 
             // Check if the unit price for the selected product and unit is available.
-            $unitPrice = $product->unitPrices()->where('unit_id', $data['unit_id'])->first();
+            $unitPrice = $product->unitPrices()->where('unit_id', $orderDetailData['unit_id'])->first();
 
             // Return a 404 error response if the unit price is not available.
             if (!$unitPrice) {
-                return response()->json([
+                $responses[] = [
                     'success' => false,
-                    'message' => 'The unit price for the selected product and unit is not available.'
-                ], 404);
+                    'id' => $orderDetailData['id'],
+                    'message' => 'The unit price for the selected product and unit is not available.',
+                ];
+                continue;
             }
 
             // Check if the new product is in the same category as the original product.
             if ($product->category_id !== $orderDetail->product->category_id) {
                 // Return an error response if the new product is not in the same category as the original product.
-                return response()->json([
+                $responses[] = [
                     'success' => false,
+                    'id' => $orderDetailData['id'],
                     'message' => sprintf(
                         'The "%s" product (category: "%s") cannot be used to replace the "%s" product (category: "%s"). The new product must be in the same category as the original product.',
                         $product->name,
@@ -121,58 +126,48 @@ class OrderDetailsController extends Controller
                         $orderDetail->product->name,
                         $orderDetail->product->category->name
                     ),
-                ], 422);
+                ];
+                continue;
             }
 
-            if ($data['operation'] === 'update') {
+            if ($orderDetailData['operation'] === 'update') {
                 // Calculate the new price based on the quantity and unit price.
-                $price = $data['quantity'] * $unitPrice->price;
+                $price = $orderDetailData['quantity'] * $unitPrice->price;
 
                 // Update the order detail with the new values.
-                $orderDetail->fill($data);
-                $orderDetail->product_id = $data['product_id'];
-                $orderDetail->unit_id = $data['unit_id'];
+                $orderDetail->fill($orderDetailData);
+                $orderDetail->product_id = $orderDetailData['product_id'];
+                $orderDetail->unit_id = $orderDetailData['unit_id'];
                 $orderDetail->price = $price;
                 $orderDetail->save();
-            } elseif ($data['operation'] === 'destroy') {
+
+                $responses[] = [
+                    'success' => true,
+                    'id' => $orderDetailData['id'],
+                    'message' => 'Order detail updated successfully',
+                ];
+            } elseif ($orderDetailData['operation'] === 'destroy') {
                 // Delete the order detail.
                 $orderDetail->delete();
+
+                $responses[] = [
+                    'success' => true,
+                    'id' => $orderDetailData['id'],
+                    'message' => 'Order detail deleted successfully',
+                ];
             } else {
-                // Return a 422 error response if the operation is invalid.
-                return response()->json([
+                // Return an error response if the operation is invalid.
+                $responses[] = [
                     'success' => false,
-                    'message' => 'Invalid operation: ' . $data['operation']
-                ], 422);
+                    'id' => $orderDetailData['id'],
+                    'message' => 'Invalid operation: ' . $orderDetailData['operation'],
+                ];
             }
-
-            // Commit the transaction.
-            DB::commit();
-
-            // Return a success response with the updated order detail.
-            if ($data['operation'] === 'update') {
-                return response()->json([
-                    'success' => true,
-                    'orderDetail' =>  $orderDetail,
-                    'message' => 'Order detail updated successfully'
-                ]);
-            } else {
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Order detail removed successfully'
-                ]);
-            }
-        } catch (\Exception $ex) {
-            // Roll back the transaction and return a 500 error response if an exception occurs while updating the order detail.
-            DB::rollBack();
-
-            return response()->json([
-                'success' => false,
-                'message' => 'An error occurred while updating the order detail: ' . $ex->getMessage()
-            ], 500);
         }
+
+        // Return the array of responses.
+        return response()->json($responses);
     }
-
-
 
     /**
      * Remove the specified resource from storage.
