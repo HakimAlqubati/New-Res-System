e2cd0597f7a34e46cb170b9525ad2f927bf122b9
Hakeem Qubati <hakimahmed123321@gmail.com>
2023-07-04 18:39:54 +0300

    completed products report with V1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ProductResource;
 use App\Models\Category;
 use App\Models\Product;
+use App\Repositories\Products\ProductRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,21 +17,16 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    private $productRepository;
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     public function index(Request $request)
     {
-        // Get the value of the ID and category ID filters from the request, or null if they're not set.
-        $id = $request->input('id');
-        $categoryId = $request->input('category_id');
-
-        // Query the database to get all active products, or filter by ID and/or category ID if they're set.
-        $products = Product::active()->when($id, function ($query) use ($id) {
-            return $query->where('id', $id);
-        })->when($categoryId, function ($query) use ($categoryId) {
-            return $query->where('category_id', $categoryId);
-        })->get();
-
-        // Return a collection of product resources.
-        return ProductResource::collection($products);
+        return $this->productRepository->index($request);
     }
 
     /**
@@ -103,51 +99,6 @@ class ProductController extends Controller
      */
     public function reportProducts()
     {
-        $from_date = $_GET['from_date'] ?? null;
-        $to_date = $_GET['to_date'] ?? null;
-        $branch_id = $_GET['branch_id'] ?? null;
-        $strSelect = 'SELECT DISTINCT 
-            products.id as product_id,
-            products.name as product_name,
-            orders_details.unit_id as unit_id,
-            units.name as unit_name,
-            COUNT(orders_details.product_id) as count,
-            orders.branch_id as branch_id,
-            branches.name as branch_name
-            -- , DATE(orders.created_at) as created_date
-        FROM 
-            products 
-            INNER JOIN orders_details ON (products.id = orders_details.product_id)
-            INNER JOIN orders ON (orders.id = orders_details.order_id)
-            inner join branches on (orders.branch_id = branches.id)
-            INNER JOIN units ON (orders_details.unit_id = units.id)';
-        $params = array();
-        if ($from_date && $to_date) {
-            $strSelect .= ' WHERE orders.created_at BETWEEN ? AND ?';
-            $params[] = $from_date;
-            $params[] = $to_date;
-        } elseif ($from_date) {
-            $strSelect .= ' WHERE orders.created_at >= ?';
-            $params[] = $from_date;
-        } elseif ($to_date) {
-            $strSelect .= ' WHERE orders.created_at <= ?';
-            $params[] = $to_date;
-        }
-        if ($branch_id) {
-            $strSelect .= ' WHERE orders.branch_id = ?';
-        }
-        $strSelect .= ' GROUP BY 
-            products.id,
-            products.name,
-            orders_details.unit_id,
-            units.name,
-            -- DATE(orders.created_at),
-           -- orders.branch_id,
-            branches.name
-        ORDER BY 
-            products.id ASC';
-            // dd($strSelect);
-        $results = DB::select($strSelect, $params);
-        return $results;
+        return $this->productRepository->report();
     }
 }
---
+++ app/Repositories/Products/ProductRepository.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Repositories\Products;
+
+use App\Http\Resources\ProductResource;
+use App\Interfaces\Products\ProductRepositoryInterface;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+
+    protected $model;
+
+    public function __construct(Product $model)
+    {
+        $this->model = $model;
+    }
+
+    function index($request)
+    {
+        // Get the value of the ID and category ID filters from the request, or null if they're not set.
+        $id = $request->input('id');
+        $categoryId = $request->input('category_id');
+
+        // Query the database to get all active products, or filter by ID and/or category ID if they're set.
+        $products = Product::active()->when($id, function ($query) use ($id) {
+            return $query->where('id', $id);
+        })->when($categoryId, function ($query) use ($categoryId) {
+            return $query->where('category_id', $categoryId);
+        })->get();
+
+        // Return a collection of product resources.
+        return ProductResource::collection($products);
+    }
+    function report()
+    {
+        $from_date = $_GET['from_date'] ?? null;
+        $to_date = $_GET['to_date'] ?? null;
+        $month = $_GET['month'] ?? null;
+        $year = $_GET['year'] ?? null;
+        $branch_id = $_GET['branch_id'] ?? null;
+        $strSelect = 'SELECT DISTINCT 
+        products.id as product_id,
+        products.name as product_name,
+        orders_details.unit_id as unit_id,
+        units.name as unit_name,
+        COUNT(orders_details.product_id) as count,
+        orders.branch_id as branch_id,
+        branches.name as branch_name
+        FROM 
+        products 
+        INNER JOIN orders_details ON (products.id = orders_details.product_id)
+        INNER JOIN orders ON (orders.id = orders_details.order_id)
+        inner join branches on (orders.branch_id = branches.id)
+        INNER JOIN units ON (orders_details.unit_id = units.id)';
+        $params = array();
+        $where = array();
+        if ($from_date && $to_date) {
+            $where[] = 'DATE(orders.created_at) BETWEEN ? AND ?';
+            $params[] = $from_date;
+            $params[] = $to_date;
+        }
+
+        if ($year && $month) {
+            $where[] = 'YEAR(orders.created_at) = ? AND MONTH(orders.created_at) = ?';
+            $params[] = $year;
+            $params[] = $month;
+        }
+
+        if ($branch_id) {
+            $where[] = 'orders.branch_id = ?';
+            $params[] = $branch_id;
+        }
+        if (!empty($where)) {
+            $strSelect .= ' WHERE ' . implode(' AND ', $where);
+        }
+        $strSelect .= ' GROUP BY 
+                products.id,
+                products.name,
+                orders_details.unit_id,
+                units.name,
+                orders.branch_id,
+                branches.name
+                ORDER BY 
+                products.id ASC';
+        $results = DB::select($strSelect, $params);
+        return $results;
+    }
+}
