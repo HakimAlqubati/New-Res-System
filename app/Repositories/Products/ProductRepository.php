8802f26dd44837a705c87721e5bb602bcb669cc5
Hakeem Qubati <hakimahmed123321@gmail.com>
2023-07-26 16:05:51 +0300

    add currency with price

--- app/Repositories/Products/ProductRepository.php
+++ app/Repositories/Products/ProductRepository.php
@@ -138,8 +138,8 @@ class ProductRepository implements ProductRepositoryInterface
             $obj = new \stdClass();
             $obj->category_id = $cat_id;
             $obj->category_name = $cat_name;
-            $obj->available_quantity =  isset($data[$cat_id]) ? $data[$cat_id]['available_quantity'] : 0;
-            $obj->price = isset($data[$cat_id]) ? $data[$cat_id]['price'] : 0;
+            $obj->available_quantity =  round(  isset($data[$cat_id]) ? $data[$cat_id]['available_quantity'] : 0,2) ;
+            $obj->price = (isset($data[$cat_id]) ? $data[$cat_id]['price'] : 0) . ' ' . 'MR';
             $final_result[] = $obj;
         }
 
@@ -179,8 +179,8 @@ class ProductRepository implements ProductRepositoryInterface
                 // 'orders_details.available_quantity',
                 'products.name',
                 'units.name',
-    //             DB::raw('YEAR(orders.created_at)'),
-    // DB::raw('MONTH(orders.created_at)')
+                //             DB::raw('YEAR(orders.created_at)'),
+                // DB::raw('MONTH(orders.created_at)')
             )
             ->get([
                 'products.category_id',
@@ -188,7 +188,7 @@ class ProductRepository implements ProductRepositoryInterface
                 'products.name as product_name',
                 'units.name as unit_name',
                 'orders_details.unit_id as unit_id',
-               DB::raw('SUM(orders_details.available_quantity) as available_quantity') 
+                DB::raw('SUM(orders_details.available_quantity) as available_quantity')
             ]);
         return $data;
     }
