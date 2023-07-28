15428243fe1bee1d781785b2a34d3f9cd8ffdf2f
Hakeem Qubati <hakimahmed123321@gmail.com>
2023-07-28 05:58:22 +0300

    add helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -7,7 +7,26 @@ function getName()
     return 'Eng. Hakeem';
 }
 
+/**
+ * to format money
+ */
 function formatMoney($amount, $currency = '$')
 {
-    return $currency .' ' . number_format($amount, 2);
+    return $currency . ' ' . number_format($amount, 2);
+}
+
+/**
+ * to return current role
+ */
+function getCurrentRole()
+{
+    return auth()->user()?->roles[0]?->id;
+}
+
+/**
+ * to get branch id
+ */
+function getBranchId()
+{
+    return auth()->user()?->branch?->id;
 }
