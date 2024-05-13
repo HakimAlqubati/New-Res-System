646e11e1e5e7748a939ee24f11933b5e9666704f
Hakeem Qubati <hakimahmed123321@gmail.com>
2024-05-13 21:44:08 +0300

    Update system_settings.php

--- resources/lang/en/system_settings.php
+++ resources/lang/en/system_settings.php
@@ -3,7 +3,7 @@ return [
     'website_name' => 'Website name',
     'system_settings' => 'System settings',
     'calculating_orders_price_method' => 'Calculating orders prices method',
-    'from_unit_prices' => 'Normal method (from unit prices)',
+    'from_unit_prices' => 'Fixed method',
     'fifo' => 'First in first out',
     'currency_symbol' => 'Currency',
     'completed_order_if_not_qty' => 'Allow orders when stock below 0',
