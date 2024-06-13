7cdeec4c2fe36b33e8f0cb99d780cf88c46faa1e
Hakeem Qubati <hakimahmed123321@gmail.com>
2024-06-13 23:32:37 +0300

    to fix repeated products in  order details

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\OrderController;
 use App\Models\Order;
 use App\Models\OrderDetails;
 use App\Models\UnitPrice;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,7 +36,61 @@ Route::get('/totest', function () {
 
     // return redirect(url('/admin'));
 });
+Route::get('/toviewrepeated', function () {
+    /**
+     * order IDs
+     * (71,82,84,86,89,90,91,92,95,103,104,106,107,110,111,112,115)
+     * 
+     */
+    $repeated_order_details = DB::table('orders_details')->select(
+        'order_id',
+        'product_id',
+        'id',
+        'unit_id',
+        'available_quantity',
+        'quantity',
+        'created_by'
+    )->get();
+    foreach ($repeated_order_details as $key => $value) {
+        $res[$value->order_id][$value->product_id][$value->unit_id][] = $value;
+    }
+    foreach ($res as $k => $v) {
 
+        foreach ($v as $kk => $vv) {
+            foreach ($vv as $kkk => $vvv) {
+                # code...
+                if (count($vvv) > 1) {
+                    $ress[] = $vvv;
+                }
+            }
+        }
+    }
+    // return $ress;
+    foreach ($ress as $r => $rv) {
+        $sum_qty = 0;
+        $sum_av_qty = 0;
+        foreach ($rv as $rr => $rrvv) {
+            $sum_qty += $rrvv->quantity;
+            $sum_av_qty += $rrvv->available_quantity;
+
+            if ($rr == 0) {
+                OrderDetails::find($rrvv->id)->delete();
+            }
+            if ($rr == 1) {
+                OrderDetails::find($rv[1]->id)->update([
+                    'quantity' => $sum_qty,
+                    'available_quantity' => $sum_av_qty,
+                ]);
+                // $ressss[$rrvv->order_id][] = [
+                //     'id' => $rrvv->id,
+                //     'sum_qty' => $sum_qty,
+                //     'sum_av_qty' => $sum_av_qty,
+                // ];
+            }
+        }
+    } 
+    return $ress;
+});
 Route::get('/', function () {
 
     return redirect(url('/admin'));
