a19045918540325495c4ca71adfd73a6fb63d886
Hakeem Qubati <hakimahmed123321@gmail.com>
2024-07-18 23:58:58 +0300

    change transfer_date type into timestamp

--- database/migrations/2024_07_13_030532_add_transfer_date_to_orders_table.php
+++ database/migrations/2024_07_13_030532_add_transfer_date_to_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->date('transfer_date')->nullable();
+            $table->timestamp('transfer_date')->nullable();
         });
     }
 
