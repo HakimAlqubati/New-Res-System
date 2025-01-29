6dc0c94db4eebbe696455f0f3f7e0c510087b7f6
HakimAlqubati <hakimahmed123321@gmail.com>
2025-01-29 11:12:26 +0300

    fix

--- config/cors.php
+++ config/cors.php
@@ -17,19 +17,18 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*'],
 
-
-    'allowed_origins' => ['https://mobile.ressystem.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Authorization', 'Content-Type', 'Accept'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
